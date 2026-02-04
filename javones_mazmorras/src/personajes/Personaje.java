f437d0dd5448b4ade3565a0a5258a9c5b13d3edf
julen <julenartesona@gmail.com>
2026-02-04 21:32:00 +0100

    Commit = personaje
    Iniciada clase base Personaje

---
+++ javones_mazmorras/src/personajes/Personaje.java
@@ -0,0 +1,17 @@
+package personajes;
+
+public class Personaje {
+	protected String nombre;
+	protected int nivel;
+	protected int puntosVida;
+	
+	
+	public Personaje (String nombre, int nivle, int puntosVida) {
+		this.nombre = nombre;
+		this.nivel = nivel;
+		this.puntosVida = puntosVida;
+	}
+	
+	
+	
+}
