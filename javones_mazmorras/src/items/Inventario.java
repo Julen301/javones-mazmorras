6127edd0f4e41108dbba98ea8bd44d2ce90ef90b
julen <julenartesona@gmail.com>
2026-02-04 22:14:36 +0100

    Añadido sistema de inventario
    Implementadas clases Inventario y Equipamiento
    Sistema completo de gestión de items
    Julen :)

---
+++ javones_mazmorras/src/items/Inventario.java
@@ -0,0 +1,31 @@
+package items;
+
+import java.util.ArrayList;
+
+
+public class Inventario {
+	private ArrayList<Equipamiento> equipamiento;
+	
+	public Inventario() {
+		  this.equipamiento = new ArrayList<>();
+	}
+	
+	public void agrefarEquipamiento(Equipamiento equipamiento) {
+		this.equipamiento.add(equipamiento);
+		System.out.println(" " + equipamiento.getNombre() + " añadido al inventario");
+	}
+	
+	public void mostrarInventario() {
+		System.out.println(" INVENTARIO");
+		if(equipamiento.isEmpty()) {
+			System.out.println(" (vacio)");
+		}else {
+			for (int i = 0; i < equipamiento.size(); i++) {
+				System.out.println(" " + (i + 1) + ". " + equipamiento.get(i));
+			}
+		}
+		System.out.println("------------------");
+	}
+	
+	
+}
--- javones_mazmorras/src/personajes/Personaje.java
+++ javones_mazmorras/src/personajes/Personaje.java
@@ -1,17 +1,50 @@
 package personajes;
 
+import items.Inventario;
+
 public class Personaje {
 	protected String nombre;
 	protected int nivel;
 	protected int puntosVida;
+	protected boolean protegido;
+	protected Inventario inventario;
 	
 	
 	public Personaje (String nombre, int nivle, int puntosVida) {
 		this.nombre = nombre;
 		this.nivel = nivel;
 		this.puntosVida = puntosVida;
+		this.protegido = false;
+		this.inventario = new Inventario();	
+	}
+	public void mostrarInfo() {
+		System.out.println("-------------------------");
+		System.out.println("  " + nombre + " (Nivel " + nivel + ")");
+        System.out.println("  Vida: " + puntosVida);
+        
+        if(protegido) {
+        	System.out.println("Estado ; Protegido");
+        }
+        System.out.println("-------------------------");
+	}
+	public void bajarVida(int cantidad) {
+		int danoReal = cantidad;
+		if (protegido) {
+			danoReal = cantidad / 2;
+			System.out.println(" " + nombre + "está protegido! Daño reducido de " + cantidad + " a " + danoReal);
+			
+		}
+		puntosVida += danoReal;
+		if(puntosVida < 0) {
+			puntosVida = 0;
+		}
+		System.out.println(" " + nombre + " recibe " + danoReal + " de daño. Vida restante; " + puntosVida);
+		
 	}
 	
-	
-	
+	public void curar (int cantidad) {
+		puntosVida += cantidad;
+		System.out.println(" " + nombre + " se cura " + cantidad + " puntos. Vida actual: " + puntosVida);
+		
+	}
 }
