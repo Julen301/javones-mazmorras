7eb20e56e96cacad8267b42183ba3726cdd94d86
Marcel <marcel.codiperpi@alumnes.ilerna.com>
2026-02-06 15:55:23 +0100

    Creada clase Guerrero con protección
    Implementado sistema de protección de compañeros

--- javones_mazmorras/src/guerrero/Guerrero.java
+++ javones_mazmorras/src/guerrero/Guerrero.java
@@ -1,5 +1,42 @@
 package guerrero;
 
-public class Guerrero {
+public class Guerrero extends Personaje {
+    private Personaje companeroProtegido;
 
+    public Guerrero(String nombre, int nivel, int puntosVida) {
+        super(nombre, nivel, puntosVida);
+        this.companeroProtegido = null;
+    }
+
+    public void proteger(Personaje companero) {
+        if (companeroProtegido != null) {
+            dejarDeProteger();
+        }
+        this.companeroProtegido = companero;
+        companero.setProtegido(true);
+        System.out.println("🛡 " + nombre + " ahora protege a " + companero.getNombre());
+    }
+
+    public void dejarDeProteger() {
+        if (companeroProtegido != null) {
+            companeroProtegido.setProtegido(false);
+            System.out.println("🛡 " + nombre + " deja de proteger a " + companeroProtegido.getNombre());
+            companeroProtegido = null;
+        } else {
+            System.out.println("⚠ " + nombre + " no está protegiendo a nadie");
+        }
+    }
+
+    public Personaje estaProtegiendo() {
+        return companeroProtegido;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        if (companeroProtegido != null) {
+            System.out.println("  🛡 Protegiendo a: " + companeroProtegido.getNombre());
+        }
+        System.out.println("═══════════════════════════════");
+    }
 }
