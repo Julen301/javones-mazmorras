aea5f03aea9ce60aeec229a6df6878b82bda9f86
Julen301 <julenartesona@gmail.com>
2026-02-06 16:30:18 +0100

    Create Main.java

---
+++ javones_mazmorras/src/Main.java
@@ -0,0 +1,67 @@
+package Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("╔═══════════════════════════════════════════╗");
+        System.out.println("║             JAVONES Y MAZMORRAS           ║");
+        System.out.println("╚═══════════════════════════════════════════╝");
+        
+        System.out.println("---CREANDO GRUPO DE AVENTUREROS---");
+        
+        Guerrero guerrero = new Guerrero("Thorin", 5, 150);
+        Mago mago = new Mago("Gandalf" , 5, 80, 100);
+        Ladron ladron = new Ladron("Bilbo" , 5, 100);
+        
+        guerrero.mostrarInfo();
+        mago.mostrarInfo();
+        ladron.mostrarInfo();
+
+        System.out.println("--- EQUIPANDO GRUPO DE AVENTUREROS ---");
+        
+        guerrero.agregarEquipamiento(new Equipamiento("Espada", "Arma"));
+        mago.agregarEquipamiento(new Equipamiento("Báculo", "Arma"));
+        ladron.agregarEquipamiento(new Equipamiento("Dagas", "Arma"));
+        
+        System.out.println("--- INVENTARIOS ---");
+        guerrero.getInventario().mostrarInventario();
+        mago.getInventario().mostrarInventario();
+        ladron.getInventario().mostrarInventario();
+        
+        System.out.println("╔═══════════════════════════════════════════╗");
+        System.out.println("║            ESCENARIO: COMBATE             ║");
+        System.out.println("╚═══════════════════════════════════════════╝");
+        
+        System.out.println("Ataque de Goblins");
+        
+        guerrero.proteger(mago);
+        mago.bajarVida(40);
+        guerrero.bajarVida(30);
+        
+        mago.lanzarHechizo(30);
+        ladron.hacerseelInvisible();
+        ladron.robar();
+        
+        System.out.println("--- DESCANSO ---");
+        
+        guerrero.dejarDeProteger();
+        guerrero.curar(20);
+        mago.curar(15);
+        mago.recargarMana(40);
+        
+        System.out.println("╔═══════════════════════════════════════════╗");
+        System.out.println("║               ESTADO FINAL                ║");
+        System.out.println("╚═══════════════════════════════════════════╝");
+        
+        guerrero.mostrarInfo();
+        mago.mostrarInfo();
+        ladron.mostrarInfo();
+        
+
+        System.out.println("╔═══════════════════════════════════════════╗");
+        System.out.println("║                VICTORIA!!                 ║");
+        System.out.println("╚═══════════════════════════════════════════╝");
+	}
+
+}
