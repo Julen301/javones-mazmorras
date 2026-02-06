1df4c4640750e8fe096b3eb8c03ecc4e1b935964
Julen301 <julenartesona@gmail.com>
2026-02-06 16:32:21 +0100

    comit de comprobacion
    sefini

--- javones_mazmorras/src/personajes/Ladron.java
+++ javones_mazmorras/src/personajes/Ladron.java
@@ -1,43 +1,41 @@
-package Ladron;
+package personajes;
 
-public class Ladron extends Personaje{
+public class Ladron extends Personaje {
 	private boolean invisible;
-	
+
 	public Ladron(String nombre, int nivel, int puntosVida) {
 		super(nombre, nivel, puntosVida);
 		this.invisible = false;
 	}
-	
+
 	public void robar() {
-		if(invisible) {
+		if (invisible) {
 			System.out.println(nombre + " Roba sigilosamente sin ser detectado!");
-		}else {
+		} else {
 			System.out.println(nombre + " Intenta robar, pero es visible!");
 		}
 	}
-	
+
 	public void hacerelInvisible() {
-		invisible =! invisible;
+		invisible = !invisible;
 		if (invisible) {
-            System.out.println( nombre + " se vuelve invisible!");
-        } else {
-            System.out.println( nombre + " vuelve a ser visible");
-        }
+			System.out.println(nombre + " se vuelve invisible!");
+		} else {
+			System.out.println(nombre + " vuelve a ser visible");
+		}
 	}
+
 	public boolean estaInvisible() {
 		return invisible;
 	}
-	@Overrride
+
+	@Override
 	public void mostrarInfo() {
-		
-	super.mostrarInfo();
-	if(invisible) {
-		System.out.println("Estado: INVISIBLE");
-	}
-	}
-	
 
+		super.mostrarInfo();
+		if (invisible) {
+			System.out.println("Estado: INVISIBLE");
+		}
 	}
-	
-	
 
+}
